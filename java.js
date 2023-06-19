220d912b2ced32d6617992073ee8223b5a959638
Joaquin-Rodriguez-grossi <136989886+Joaquin-Rodriguez-grossi@users.noreply.github.com>
2023-06-19 20:41:57 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,36 @@
+
+const btnDesc1 = document.getElementById('btnDesc1');
+const img1 = document.getElementById('img1');
+const desc = document.getElementById('desc1');
+
+btnDesc1.onclick = () => {
+    img1.classList.toggle('active');
+    desc1.classList.toggle('active');
+}
+
+const btnDesc2 = document.getElementById('btnDesc2');
+const img2 = document.getElementById('img2');
+const desc2 = document.getElementById('desc2');
+
+btnDesc2.onclick = () => {
+    img2.classList.toggle('active');
+    desc2.classList.toggle('active');
+}
+
+const btnDesc3 = document.getElementById('btnDesc3');
+const img3 = document.getElementById('img3');
+const desc3 = document.getElementById('desc3');
+
+btnDesc3.onclick = () => {
+    img3.classList.toggle('active');
+    desc3.classList.toggle('active');
+}
+
+const btnDesc4 = document.getElementById('btnDesc4');
+const img4 = document.getElementById('img4');
+const desc4 = document.getElementById('desc4');
+
+btnDesc4.onclick = () => {
+    img4.classList.toggle('active');
+    desc4.classList.toggle('active');
+}
